3c6c3d451475548e684fc3e83ecf415caef82c9d
Julian Reales <jreales0125@gmail.com>
2022-03-19 21:17:45 -0500

    Termino el segundo taller

--- descuentod.js
+++ descuentod.js
@@ -1,1 +1,29 @@
-console.log("hola mudno")
+//const precioOriginal = 100;
+//const descuento = 18;
+
+function calcularPrecioConDescuento(precio, descuento) {
+  const porcentajePrecioConDescuento = 100 - descuento;
+  const precioConDescuento = precio * (porcentajePrecioConDescuento / 100);
+
+  return precioConDescuento;
+}
+
+function onClickButonPriceDiscount() {
+  const inputPrice = document.getElementById("InputPrice");
+  const priceValue = inputPrice.value;
+
+  const inputDiscount = document.getElementById("InputDiscount");
+  const discountValue = inputDiscount.value;
+
+  const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+  const resultP = document.getElementById("ResultP");
+  resultP.innerText = "El precio con descuento es: $" + precioConDescuento;
+}
+
+//console.log({
+  //precioOriginal,
+  //descuento,
+  //porcentajePrecioConDescuento,
+  //precioConDescuento,
+//});
